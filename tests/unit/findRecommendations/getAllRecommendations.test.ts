d3a6cd3d089473d3e05b6a80439347ddc4e3d014
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-24 19:40:18 -0300

    :test_tube: test: Updanting unit test for the get recommendations

--- tests/unit/findRecommendations/getAllRecommendations.test.ts
+++ tests/unit/findRecommendations/getAllRecommendations.test.ts
@@ -1,3 +1,4 @@
+import { Recommendation } from "@prisma/client";
 import { recommendationRepository } from "../../../src/repositories/recommendationRepository";
 import { recommendationService } from "../../../src/services/recommendationsService";
 import * as recommendationsFactory from "../../factory/recommendationsFactory";
@@ -9,17 +10,19 @@ beforeEach(() => {
 
 describe("Get all recommendations", () => {
   it("Should return a list with all recommendations", async () => {
-    const recommendations = recommendationsFactory.__allRecommendations();
-            console.log(recommendations) 
+    const recommendations: Recommendation[] =
+      recommendationsFactory.__allRecommendations();
 
     jest
       .spyOn(recommendationRepository, "findAll")
       .mockResolvedValueOnce(recommendations);
 
-    const allRecommendations = await recommendationService.get();
+    const allRecommendations: Recommendation[] =
+      await recommendationService.get();
 
     expect(recommendationRepository.findAll).toHaveBeenCalled();
     expect(allRecommendations).not.toBeFalsy();
     expect(allRecommendations).toBeInstanceOf(Array);
+    expect(allRecommendations).toEqual(recommendations)
   });
 });
