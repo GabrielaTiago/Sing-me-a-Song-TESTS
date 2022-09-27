7fb52b16d6d5305c00ef07d7bc8d970d774db739
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-26 23:47:01 -0300

    :heavy_check_mark: test: E2E test to get the top recommendations filtered by the amount

---
+++ FRONTEND/cypress/e2e/getTopRecommendations.cy.js
@@ -0,0 +1,39 @@
+import { Chance } from "chance";
+import { __input } from "../factory/recommendationsFactory";
+
+const chance = Chance();
+
+beforeEach(async () => {
+  await cy.resetDatabase();
+});
+
+describe("GET the recommendations", () => {
+  it("Should return a message when has no recommendations", () => {
+    cy.visit("/");
+    cy.contains("Loading...");
+  });
+
+  it("Should get the top recommendations, filtered by the amount", () => {
+    const amount = chance.integer({ min: 1, max: 15 });
+    const allRecommendations = [];
+
+    for (let i = 0; i < amount; i++) {
+      const recommendation = __input();
+      allRecommendations.push(recommendation);
+      cy.createRecommendation(recommendation);
+    }
+    cy.visit("/");
+
+    cy.get("[data-cy='top']")
+    cy.intercept("GET", `/recommendations/top/${amount}`).as("getRecommendations");
+
+    cy.get("[data-cy = 'recommendation']").should(
+      "have.length.of.at.most",
+      amount
+    );
+
+    allRecommendations.forEach((item) => {
+      cy.contains(item.name).should("be.visible");
+    });
+  });
+});
--- FRONTEND/src/components/Menu/Menu.js
+++ FRONTEND/src/components/Menu/Menu.js
@@ -15,7 +15,7 @@ export default function Menu() {
         Home
       </MenuItem>
 
-      <MenuItem onClick={() => navigate("/top")}>
+      <MenuItem data-cy="top" onClick={() => navigate("/top")}>
         <HiTrendingUp />
         Top
       </MenuItem>
