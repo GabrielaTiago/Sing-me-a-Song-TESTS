e69cf2371277ce313e6d5d1b7d6a97fc47bf0183
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-26 12:03:18 -0300

    :gear: config: Update Cypress configuration to use environment variables

--- FRONTEND/cypress.config.js
+++ FRONTEND/cypress.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+require('dotenv').config();
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000/',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	e2e: {
+		baseUrl: process.env.REACT_APP_BASE_URL,
+		env: {
+			apiUrl: process.env.REACT_APP_API_BASE_URL,
+		},
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 });
