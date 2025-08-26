fcf1bdc6f0c3407417e19b3fac85a0cb91f4fc5e
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-26 19:36:21 -0300

    :gear: config: Update test setup configuration by adding dotenv for env variables

--- BACKEND/tests/setup.ts
+++ BACKEND/tests/setup.ts
@@ -1,7 +1,10 @@
 import { execSync } from 'child_process';
+import dotenv from 'dotenv';
+import path from 'path';
 
 export default async () => {
   console.log('Configuring test database...');
+  dotenv.config({ path: path.resolve(__dirname, '..', '.env.test') });
   execSync('npx prisma generate && npx prisma migrate deploy');
   console.log('Test database configured successfully!');
 };
