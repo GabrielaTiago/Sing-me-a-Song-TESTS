fc3c74b0f5a32ac0d1b8ec52276fe9a1ca6eb302
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-27 14:23:41 -0300

    :books: docs: Integrate Swagger UI

--- BACKEND/src/routers/index.ts
+++ BACKEND/src/routers/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from '../config/swagger';
 import recommendationRouter from './recommendationRouter';
 import testRouter from './e2eRouter';
 
 const router = Router({ mergeParams: true });
 
+router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 router.use('/recommendations', recommendationRouter);
 
 if (process.env.NODE_ENV === 'test') {
